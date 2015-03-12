3875c28525319fa7961c727bde6cfb885e4b6d74
Anne <apzo24@yahoo.com>
2015-03-11 20:32:22 -0700

    Completed functions

--- 06-functions/00-basics/challenge.js
+++ 06-functions/00-basics/challenge.js
@@ -1,3 +1,9 @@
-module.exports.helloWorld = undefined;
+module.exports.helloWorld = function() { 
+    return "Hello World";
+  }
+ 
 
-module.exports.multiply = undefined;
+module.exports.multiply = function(numberX, numberY) {
+    return numberX * numberY;
+  }
+ 
