5f8122d9c54e21a2f128831cbfb7584cf178996c
Anne <apzo24@yahoo.com>
2015-03-12 21:41:00 -0700

    Completed if-statements

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,45 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(guessNumber, favoriteNumber) {
+  if (guessNumber > favoriteNumber) {
+    return "Too low";
+  }
+  else if (guessNumber < favoriteNumber) {
+    return "Too high";
+  }
+  else if (guessNumber == favoriteNumber) {
+    return "You got it!";
+  }
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(x, y, z, w) {
+  if (x != 3 && x != 5 && x != 7) {
+    return "incorrect";
+  }
+  else if (y != 2) {
+    return "incorrect";
+  }
+  else if (z < 5 || z > 100) {
+    return "incorrect";
+  }
+  else if (w >= 9 && w <= 20) {
+    return "incorrect";
+  }
+  else {
+    return "correct";
+  }
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  if (item == "MacBook Air" && money >= 999) {
+    return true;
+  }
+  else if (item == "MacBook Pro" && money >= 1299) {
+    return true;
+  }
+  else if (item == "Mac Pro" && money >= 2499) {
+    return true;
+  }
+  else if (item == "Apple Sticker" && money >= 1) {
+    return true;
+  }
+  return false;
+  }
