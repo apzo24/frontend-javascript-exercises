ac3ba6c1691a37d5d23ae2756124a55804ddff58
Anne <apzo24@yahoo.com>
2015-03-20 20:02:34 -0700

    Completed objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,31 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var x = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  }
+  return x;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if (object[newProp] === undefined) {
+    object[newProp] = newValue;
+  }
+  return object;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+    var list = {
+      'MacBook Air': 999,
+      'MacBook Pro': 1299,
+      'Mac Pro': 2499,
+      'Apple Sticker': 1
+    };
+
+    amount = list[item];
+    return (money >= amount && amount !== undefined);
+};
